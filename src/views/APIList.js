ae2eefea37157e09e77290c07233809125670ed2
James Demiraiakian <james.armen@gmail.com>
2022-01-25 15:49:06 -0800

    working category select

--- src/views/APIList.js
+++ src/views/APIList.js
@@ -6,34 +6,43 @@ import './APIList.css';
 export default function APIList() {
   const [api, setApi] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [category, setCategory] = useState('');
+  const [category, setCategory] = useState('All');
   const [catList, setCatList] = useState([]);
 
   useEffect(() => {
     const fetchCat = async () => {
       const categories = await fetchData('categories');
       setCatList(categories);
-      console.log(catList);
     };
     fetchCat();
   }, []);
+  console.log(catList);
 
   useEffect(() => {
     const fetchApi = async () => {
-      const data = await fetchData('entries');
-      setApi(data.entries);
-      setLoading(false);
+      if (category === 'All') {
+        const data = await fetchData('entries');
+        setApi(data.entries);
+        setLoading(false);
+      } else {
+        const data = await fetchData(`entries?category=${category}`);
+        setApi(data.entries);
+        setLoading(false);
+      }
     };
     fetchApi();
-  }, []);
+  }, [category]);
 
   if (loading) return <h1>LOADING</h1>;
 
   return (
     <>
       <select value={category} onChange={(e) => setCategory(e.target.value)}>
+        <option value="All">All</option>
         {catList.map((cat) => (
-          <option key={cat}>{cat}</option>
+          <option key={cat} value={cat}>
+            {cat}
+          </option>
         ))}
       </select>
       <div className="card-box">
