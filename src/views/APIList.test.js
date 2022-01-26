7944d7e3bb73c4e947f75d8acedad3e76d17777f
James Demiraiakian <james.armen@gmail.com>
2022-01-26 10:39:59 -0800

    Fixed tests, added more tests for coverage

--- src/views/APIList.test.js
+++ src/views/APIList.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import APIList from './APIList';
 
 //test dropdown displays
@@ -10,20 +11,20 @@ test('test for dropdown display', async () => {
 });
 
 //test for category options exist
-// test('test for dropdown display', async () => {
-//   render(<APIList />);
+test('test for dropdown display', async () => {
+  render(<APIList />);
 
-//   const option = await screen.findByRole('menuitem', { name: /all/i });
-//   expect(option).toBeInTheDocument();
-// });
+  const option = await screen.findByRole('option', { name: /all/i });
+  expect(option).toBeInTheDocument();
+});
 
 //test category select changes cards rendered
-// test('test for category behavior', async () => {
-//   render(<APIList />);
+test('test for category behavior', async () => {
+  render(<APIList />);
 
-//   const category = await screen.findByRole('combobox', { name: /books/i });
-//   userEvent.selectOptions(category);
-//   expect(screen.getByRole('menuitem', { name: 'books' }).selected).toBe(true);
-// });
+  const category = await screen.findByRole('combobox');
+  userEvent.selectOptions(category, 'Books');
+  expect(screen.getByRole('option', { name: 'Books' }).selected).toBe(true);
+});
 
 //test api tiles render
